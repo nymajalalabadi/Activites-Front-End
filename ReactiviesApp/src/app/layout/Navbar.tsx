c5018c6efce93b1edaf0772a93fecbd176a455ed
nym jalalabadi <nima.jalalabadii@gmail.com>
2025-10-02 09:55:52 -0600

    Updated Navbar

--- ReactiviesApp/src/app/layout/Navbar.tsx
+++ ReactiviesApp/src/app/layout/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Menu } from 'semantic-ui-react';
 import { useStore } from '../../stores/store';
+import { NavLink } from 'react-router-dom';
 
 
 
@@ -10,13 +11,13 @@ export default function NavBar(){
 
         <Menu inverted fixed='top'>
             <Container>
-                <Menu.Item header>
+                <Menu.Item header as={NavLink} to='/'>
                     <img src="/assets/logo.png" alt="logo" />
                     Reactivites
                 </Menu.Item>
-                <Menu.Item name='Activities'>
+                <Menu.Item name='Activities' as={NavLink} to='/activities'>
                 </Menu.Item>
-                <Menu.Item name='Activities'>
+                <Menu.Item name='Activities' as={NavLink} to='/createActivity'>
                     <button onClick={()=>activityStore.openForm()} className="ui positive button">Create Activity</button>
                 </Menu.Item>
             </Container>
